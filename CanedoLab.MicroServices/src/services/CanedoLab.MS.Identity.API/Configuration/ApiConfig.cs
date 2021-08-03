ecef5060d9b1cd40241c6109af676157252450f0
Flavio Canedo <flavio_canedo@live.com>
2021-08-03 02:40:19 -0300

    Create Identity Api

---
+++ CanedoLab.MicroServices/src/services/CanedoLab.MS.Identity.API/Configuration/ApiConfig.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace CanedoLab.MS.Identity.API.Configuration
+{
+    public static class ApiConfig
+    {
+        public static IServiceCollection AddApiConfig(this IServiceCollection services) 
+        {
+            services.AddControllers();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseApiConfig(this IApplicationBuilder app, IWebHostEnvironment env) 
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseIdentityConfig();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            return app;
+        }
+    }
+}
---
+++ CanedoLab.MicroServices/src/services/CanedoLab.MS.Identity.API/Configuration/IdentityConfig.cs
@@ -0,0 +1,61 @@
+﻿using CanedoLab.MS.Identity.API.Data;
+using CanedoLab.MS.Identity.API.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace CanedoLab.MS.Identity.API.Configuration
+{
+    public static class IdentityConfig 
+    {
+        public static IServiceCollection AddIdentityConfig(this IServiceCollection services, IConfiguration configuration) 
+        {
+            services
+                .AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders()
+                .AddErrorDescriber<IdentityErrorDescriberPortuguese>();
+
+            var appSettingsSection = configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(configureOptions =>
+            {
+                configureOptions.RequireHttpsMetadata = true;
+                configureOptions.SaveToken = true;
+                configureOptions.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(appSettings.Secret)),
+
+                    ValidateIssuer = true,
+                    ValidIssuer = appSettings.Issuer,
+
+                    ValidateAudience = true,
+                    ValidAudience = appSettings.Audience
+                };
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseIdentityConfig(this IApplicationBuilder app) 
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            return app;
+        }
+    }
+}
